68ce5d2c0074803e09f194f8324cbcbf7b3311c5
Kaung Myat Soe <52688252+ZhanZhao30@users.noreply.github.com>
2021-11-17 11:24:36 +0630

    Update ExpenseItem.js

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,17 +3,12 @@ import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickExpense = () => {
-    setTitle('update!!')
-  };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickExpense}>Click Change</button>
       </div>
     </div>
   );
